2fe9bb0b4f0dd730eda3682ea6ad3c1a1ca060f3
Reazzyg <mudrsemen@ya.ru>
2024-04-01 19:26:06 +0300

    01.04.24

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,58 +1,38 @@
 class FormValidator {
   constructor(form) {
-    const self = this;
-
-    const pathToSendFile = "/system/send.php";
-
     this.form = form;
-
     this.submitButton = this.form.querySelector(".form-button");
-
     this.stars = this.form.querySelectorAll(".form-rating__star");
-
     this.ratingInput = this.form.querySelector("#ratingInput");
-
     this.emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-
     this.errorUrl = "/assets/img/error.svg";
-
     this.validUrl = "/assets/img/done.svg";
-
     this.errorText = "Введены некорректные данные";
-
     this.emptyText = "Поле обязательно для заполнения";
-
     this.inputName = this.form.querySelector('input[name="name"]');
-
     this.inputEmail = this.form.querySelector('input[name="email"]');
-
     this.inputTel = this.form.querySelector('input[name="phone"]');
 
-    this.inputName.onblur = () => this.validateName();
+    this.setupEventListeners();
+  }
 
-    this.inputTel.onblur = () => this.validateTel();
-
-    this.inputEmail.onblur = () => this.validateEmail();
-
-    this.inputTel.addEventListener("input", () => this.validateTel(this.inputTel));
-
+  setupEventListeners() {
+    this.inputName.addEventListener("blur", () => this.validateName());
+    this.inputTel.addEventListener("blur", () => this.validateTel());
+    this.inputEmail.addEventListener("blur", () => this.validateEmail());
+    this.inputTel.addEventListener("input", () => this.validateTel());
     this.stars.forEach((star) => {
       star.addEventListener("click", () => this.highlightStars(star.getAttribute("data-value")));
       star.addEventListener("mouseover", () => this.highlightStars(star.getAttribute("data-value")));
     });
-
-    this.submitButton.addEventListener("click", function (event) {
-      event.preventDefault();
-
-      self.checkIfValidated();
-    });
+    this.form.addEventListener("submit", (event) => this.handleSubmit(event));
   }
 
   highlightStars(value) {
     this.stars.forEach((star) => {
       if (star.getAttribute("data-value") <= value) {
         star.classList.add("selected");
-        ratingInput.value = value;
+        this.ratingInput.value = value;
       } else {
         star.classList.remove("selected");
       }
@@ -78,7 +58,6 @@ class FormValidator {
 
   createMessage(elem, text) {
     const label = elem.closest("label");
-
     label.querySelector(".error").textContent = text;
   }
 
@@ -88,6 +67,7 @@ class FormValidator {
       error.textContent = "";
     }
   }
+
   validateName() {
     const value = this.inputName.value.trim();
     if (value === "") {
@@ -106,32 +86,23 @@ class FormValidator {
       return false;
     }
 
-    // const isEmpty = this.checkIfEmpty(value, this.inputEmail);
-
     const isValid = this.emailPattern.test(value);
-
     const iconUrl = isValid ? this.validUrl : this.errorUrl;
 
     this.deleteIcon(this.inputEmail);
-
     this.createIcon(iconUrl, this.inputEmail);
 
     isValid ? this.deleteMessage(this.inputEmail) : this.createMessage(this.inputEmail, this.errorText);
-
     return isValid;
   }
 
   validateTel() {
     const value = this.inputTel.value.trim();
-
-    this.inputTel.value = "+" + value.replace(/\D/g, ""); // Удаляем все нецифровые символы
-
+    this.inputTel.value = "+" + value.replace(/\D/g, "");
     const maxLength = 12;
-
     if (this.inputTel.value.length > maxLength) {
       this.inputTel.value = this.inputTel.value.slice(0, maxLength);
     }
-
     if (value === "" || value === "+") {
       this.createMessage(this.inputTel, this.emptyText);
       return false;
@@ -141,47 +112,52 @@ class FormValidator {
   }
 
   checkIfValidated() {
-    this.validateName();
-    this.validateTel();
-    this.validateEmail();
+    const isValidName = this.validateName();
+    const isValidTel = this.validateTel();
+    const isValidEmail = this.validateEmail();
+    return isValidName && isValidTel && isValidEmail;
+  }
 
-    if (this.validateName() && this.validateTel() && this.validateEmail()) {
-      const formData = new FormData(this.form);
-      fetch("/system/send.php", {
-        method: "POST",
-        body: formData,
-      })
-        .then((response) => {
-          if (response.ok) {
-            console.log("success");
-          } else {
-            console.log("err");
-          }
-        })
-        .catch((err) => {
-          console.log("error:".err);
+  async sendForm(formData) {
+    if (this.checkIfValidated()) {
+      try {
+        const response = await fetch("/system/send.php", {
+          method: "POST",
+          body: formData,
         });
+        if (response && response.ok) {
+          return await response.json();
+        }
+      } catch (error) {
+        console.error("Error sending form: ", error);
+      }
+    } else {
+      return false;
     }
   }
 
-  // preventSending() {
-  //     this.form.addEventListener('submit', event => {
-  //       let isValid = true;
-  //       const emailInputs = form.querySelectorAll('.form-email');
-  //       emailInputs.forEach(input => {
-  //         if (!this.validate(input)) {
-  //           isValid = false;
-  //         }
-
-  //       if (!isValid) {
-  //         event.preventDefault();
-  //         console.log('Form submission prevented');
-  //       } else {
-  //         console.log('Form submitted successfully');
-  //       }
-  //     });
-  //   });
-  // }
+  handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(this.form);
+    this.sendForm(formData)
+      .then((response) => {
+        if (response.success === "Success") {
+          console.log("asd");
+        }
+        // if ((a = 1)) {
+        //   console.log("Form submitted successfully");
+        //   // Показать блок с галочкой
+        //   // Пример: document.getElementById("success").style.display = "block";
+        // } else {
+        //   console.log("Form submission failed");
+        //   // Показать блок с крестиком
+        //   // Пример: document.getElementById("error").style.display = "block";
+        // }
+      })
+      .catch((error) => {
+        console.error("Error sending form: ", error);
+      });
+  }
 }
 
 export default function validate(form) {
--- system/send.php
+++ system/send.php
@@ -1,28 +1,35 @@
-<?
+<?php
 include $_SERVER['DOCUMENT_ROOT'] . '/system/functions.php';
-// header("Location: {$_SERVER['/comments']}");
-
-
+header('Content-Type: application/json');
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST["name"];
-  $tel = $_POST["phone"];
-  $email = $_POST["email"];
-  $rating = $_POST["rating"];
-  $comment = $_POST["comment"];
-  $sqlComment = "INSERT INTO mail (name, phone, email, rating, comment ) VALUES ('$name', '$tel', '$email', '$rating', '$comment')";
-  $res = customSqlQuery($connect, $sqlComment);
-  if ($res === TRUE) {
-    echo "Отзыв успешно добавлен";
-    header("Location: {$_SERVER['PHP_SELF']}");
-    exit();
+  $name = htmlspecialchars($_POST["name"]);
+  $tel = htmlspecialchars($_POST["phone"]);
+  $email = htmlspecialchars($_POST["email"]);
+  $rating = htmlspecialchars($_POST["rating"]);
+  $comment = htmlspecialchars($_POST["comment"]);
+
+  // Проверяем, что все поля, кроме комментария и рейтинга, заполнены
+  if (!empty($name) && !empty($tel) && !empty($email)) {
+    // Подготавливаем SQL-запрос с использованием подготовленных выражений для предотвращения SQL-инъекций
+    $sqlComment = "INSERT INTO mail (name, phone, email, rating, comment) VALUES (?, ?, ?, ?, ?)";
+    $stmt = $connect->prepare($sqlComment);
+    $stmt->bind_param("sssis", $name, $tel, $email, $rating, $comment);
+
+    // Выполняем запрос
+    if ($stmt->execute()) {
+      // После успешной вставки перенаправляем пользователя на другую страницу
+      // header("Location: /comments/?success=true");
+      echo json_encode(array("success" => "Success"));
+      exit();
+    } else {
+      // Если возникла ошибка при выполнении запроса, выводим сообщение об ошибке
+      echo json_encode(array("error" => "Ошибка при добавлении отзыва: " . $connect->error));
+    }
   } else {
-    echo "Ошибка при добавлении отзыва: " . $connect->error;
+    // Если какое-либо из основных полей не заполнено, выводим сообщение об ошибке
+    echo json_encode(array("error" => "Поля 'Имя', 'Телефон' и 'Email' обязательны для заполнения"));
   }
-  echo "Полученные данные:<br>";
-  echo "Имя: " . $name . "<br>";
-  echo "Email: " . $email . "<br>";
-  echo "Телефон: " . $tel . "<br>";
 } else {
-  // Если запрос был выполнен не методом POST, вы можете вернуть ошибку или выполнить другие действия
-  echo "Ошибка: Неверный метод запроса";
+  // Если запрос был выполнен не методом POST, выводим сообщение об ошибке
+  echo json_encode(array("error" => "Ошибка: Неверный метод запроса"));
 }
